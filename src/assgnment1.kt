b6ad7eb4d9ceeb4ef4997715767ef5f71b6c8acb
Marie-Louise <mariemuteteyimana@gmail.com>
2021-03-24 17:33:52 +0300

    assgnment1

---
+++ src/assgnment1.kt
@@ -0,0 +1,33 @@
+fun main() {
+name()
+age()
+    weight()
+    phone()
+    citizen()
+}
+    fun name(){
+    val name="marie louise"
+    println (name)
+    }
+    fun age(){
+        var age=20
+        println(age)
+    }
+fun weight(){
+    var weight=60.545454
+    println(weight)
+}
+fun phone(){
+    var phone=+250781849006
+    println(phone)
+}
+fun citizen(){
+    val citizen="true"
+    print(citizen)
+    val student=arrayOf("marie louise",20,60.545454,"true")
+    println(student)
+
+
+
+
+}
